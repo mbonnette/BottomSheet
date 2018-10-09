a72cdd93b7403c0bec0f9bb6f01869a4fb696591
Michael Bonnette <mbonnette@mac.com>
2018-10-08 23:19:30 -0400

    Show current location and spot for first trip.. Harvard in this default case

--- BottomSheet/MapViewController.swift
+++ BottomSheet/MapViewController.swift
@@ -6,25 +6,61 @@ import MapKit
 import UIKit
 import CoreData
 
-class MapViewController: UIViewController, NSFetchedResultsControllerDelegate {
+class MapViewController: UIViewController, NSFetchedResultsControllerDelegate, CLLocationManagerDelegate {
 
     private lazy var mapView = MKMapView(frame: view.bounds)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.addSubview(mapView)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    }
-
+	private lazy var locationMgr = CLLocationManager()
+	private lazy var geoCoder = CLGeocoder()
 	private lazy var tripProvider: TripProvider = {
-		
 		let provider = TripProvider()
 		provider.fetchedResultsControllerDelegate = self
 		return provider
 	}()
+	let regionRadius: CLLocationDistance = 100000
+
 	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.locationMgr.requestWhenInUseAuthorization()
+		mapView.showsUserLocation = true
+
+		view.addSubview(mapView)
+		mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+		let initialLocation = CLLocation(latitude: 42.377806, longitude: -71.111969)
+		centerMapOnLocation(location: initialLocation)
+	}
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		checkLocationAuthorizationStatus()
+	}
+
+
+	// MARK: - CLLocationManager
 	
+	func checkLocationAuthorizationStatus() {
+		if CLLocationManager.authorizationStatus() == .authorizedAlways {
+			mapView.showsUserLocation = true
+		} else {
+			locationMgr.requestAlwaysAuthorization()
+		}
+		//    if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+		//      mapView.showsUserLocation = true
+		//    } else {
+		//      locationMgr.requestWhenInUseAuthorization()
+		//    }
+	}
+
+
+	// MARK: - Helper methods
+	
+	func centerMapOnLocation(location: CLLocation) {
+		let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+												  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+		mapView.setRegion(coordinateRegion, animated: true)
+	}
 	
 
 }
